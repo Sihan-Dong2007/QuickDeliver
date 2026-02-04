3c360441f62efafbed10968a94e32dc1bfa47250
Sihan <dong70@byu.edu>
2026-02-04 14:01:38 -0700

    import files to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Choices } from './choices/choices';
+import { Table } from './table/table';
+
 
 
 export default function App() {
