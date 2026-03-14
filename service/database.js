728b78c6a4f63fcc4267a82aeb96c02aea4d0368
Sihan <dong70@byu.edu>
2026-03-14 16:37:12 -0600

    change database.js

--- service/database.js
+++ service/database.js
@@ -1,30 +1,55 @@
-// database.js
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${encodeURIComponent(config.password)}@${config.hostname}`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
 
-let client;
 let db;
+let usersCollection;
+let ordersCollection;
 
+// 连接数据库
 async function connectDB() {
-  if (db) return db; // 已连接就直接返回
-
-  try {
-    client = new MongoClient(url);
+  if (!db) {
     await client.connect();
-
-    // 选择数据库名字
-    db = client.db('test'); // 你也可以改成你需要的名字，比如 'startup'
-    await db.command({ ping: 1 });
+    db = client.db('quickdeliver'); // 这里可以换成你自己的数据库名
+    usersCollection = db.collection('users');
+    ordersCollection = db.collection('orders');
     console.log('Database connected!');
-
-    return db;
-  } catch (err) {
-    console.error('Failed to connect to database:', err);
-    process.exit(1);
   }
+  return db;
 }
 
-// 可以选择性导出 MongoClient，如果后续想直接操作 client
-module.exports = { connectDB, client };
+// 用户操作
+async function addUser(user) {
+  await usersCollection.insertOne(user);
+}
+
+async function getUser(store) {
+  return await usersCollection.findOne({ store });
+}
+
+async function updateUserToken(store, token) {
+  await usersCollection.updateOne(
+    { store },
+    { $set: { token } }
+  );
+}
+
+// 订单操作
+async function addOrder(order) {
+  await ordersCollection.insertOne(order);
+}
+
+async function getOrders() {
+  return await ordersCollection.find().toArray();
+}
+
+module.exports = {
+  connectDB,
+  addUser,
+  getUser,
+  updateUserToken,
+  addOrder,
+  getOrders,
+};
